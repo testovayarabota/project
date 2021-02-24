f6a6e5261440f222f29ceff409f1d5df8a9157d8
testovayarabota <testovayarabota@yandex.ru>
2021-02-24 16:09:35 +0300

    testing

--- pages/login.vue
+++ pages/login.vue
@@ -6,13 +6,17 @@
               <div class="card">
                   <form class="box">
                       <h1>Login</h1>
-                      <p class="text-muted"> Please enter your login and password!</p> <input type="text" name="" placeholder="Username"> <input type="password" name="" placeholder="Password"> <a class="forgot text-muted" href="#">Forgot password?</a> <input type="submit" name="" value="Login" href="#">
+                      <p class="text-muted"> Please enter your login and password!
+                      </p> <input type="text" name="" placeholder="Username">
+                      <input type="password" name="" placeholder="Password">
+                       <a class="forgot text-muted" href="#">Forgot password?</a>
+                       <input type="submit" name="" value="Login" href="#">
+                       <li class="nav-item">
+                         <nuxt-link exact active-class="active" class="nav-link" aria-current="page" to="/">
+                          Home
+                         </nuxt-link>
+                       </li>
                       <div class="col-md-12">
-                          <ul class="social-network social-circle">
-                              <li><a href="#" class="icoFacebook" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
-                              <li><a href="#" class="icoTwitter" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-                              <li><a href="#" class="icoGoogle" title="Google +"><i class="fab fa-google-plus"></i></a></li>
-                          </ul>
                       </div>
                   </form>
               </div>
