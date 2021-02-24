d907cef4b970662576e1291589734477983bf6ff
testovayarabota <testovayarabota@yandex.ru>
2021-02-24 16:04:15 +0300

    dell

--- pages/login.vue
+++ pages/login.vue
@@ -31,15 +31,7 @@ export default {
 </script>
 
 <style>
-/*
-/*
-/* body {
-/*    margin: 0;
-/*    padding: 0;
-/*    font-family: sans-serif;
-/*    background: linear-gradient(to right, #b92b27, #1565c0)
-/*}
-/*
+
 .box {
     width: 500px;
     padding: 40px;
